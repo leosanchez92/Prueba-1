e732606c24060cc8e489856c2ccc7d65b1037ab4
leosanchez92 <nelsonsanchez.al@gmail.com>
2022-04-04 17:16:06 -0400

    Agregado PRC

---
+++ 2_PRC/data/prc_label.js
@@ -0,0 +1,23 @@
+var labPRC = {
+"type": "FeatureCollection",
+"name": "prc_label",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-2", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.951553842916127, -38.740139515868776 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZE-3", "Descripc": "Área de Extensión Urbana" }, "geometry": { "type": "Point", "coordinates": [ -72.939555110838356, -38.74907728926857 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-4", "Descripc": "Áreas Consolidadas - Ultra Chol-Chol" }, "geometry": { "type": "Point", "coordinates": [ -72.960427450985577, -38.743137504216598 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-1", "Descripc": "Zona con Riesgo de Inundación" }, "geometry": { "type": "Point", "coordinates": [ -72.957620509535261, -38.748455581228775 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-2", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.943791489937396, -38.746307963654644 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-1", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.95165431849334, -38.744958289256999 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-3", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.94302412085662, -38.751244916061367 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZE-2", "Descripc": "Área de Extensión Urbana" }, "geometry": { "type": "Point", "coordinates": [ -72.954094670780407, -38.737084539895697 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-2", "Descripc": "Faja protección No Edificable 60m Estero Pichicó" }, "geometry": { "type": "Point", "coordinates": [ -72.96056204369529, -38.737609678167701 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-2", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.941601232637268, -38.750467628639726 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZU-3", "Descripc": "Áreas Consolidadas" }, "geometry": { "type": "Point", "coordinates": [ -72.948395778211136, -38.749459878475825 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-2", "Descripc": "Faja protección No Edificable 5m ambos lados Estero Coilaco" }, "geometry": { "type": "Point", "coordinates": [ -72.947342892080073, -38.750127704296624 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-2", "Descripc": "Faja protección No Edificable 15m borde Río Chol-Chol" }, "geometry": { "type": "Point", "coordinates": [ -72.95863336630751, -38.744223068808282 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-2", "Descripc": "Faja protección No Edificable 15m borde Río Chol-Chol" }, "geometry": { "type": "Point", "coordinates": [ -72.957289314215643, -38.743994538008529 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZR-1", "Descripc": "Zona con Riesgo de Inundación" }, "geometry": { "type": "Point", "coordinates": [ -72.96077921515446, -38.745944539043265 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Zona": "ZE-1", "Descripc": "Área de Extensión Urbana" }, "geometry": { "type": "Point", "coordinates": [ -72.939929490885163, -38.740050946310873 ] } }
+]
+}
